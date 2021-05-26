df5765425000330f5d7392459010b83129065b6e
Chidambara Sundaram Palani <chidambarasundarampalani@Chidambaras-MacBook-Pro.local>
2021-05-26 09:24:22 +0530

    Loops

---
+++ Loops/While.py
@@ -0,0 +1,8 @@
+L = []
+while len(L) <=2:
+    entry = input("Enter the Name: ").strip().capitalize()
+    L.append(entry)
+    print (L)
+print("Sorry the list is full")
+print("Complete list is ")
+print(L)
