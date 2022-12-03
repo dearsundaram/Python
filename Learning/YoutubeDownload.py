b561794988210a201baefce4c1e05c1b6e00cb26
dearsundaram <54997264+dearsundaram@users.noreply.github.com>
2022-12-03 08:50:21 +0100

    Delete Learning directory

--- Learning/YoutubeDownload.py
+++
@@ -1,8 +0,0 @@
-from pytube import YouTube
-
-link = "https://youtu.be/Lrq6r_uSQac"
-yt = YouTube(link)
-print(yt.title)
-print(yt.thumbnail_url)
-yt = yt.streams.get_highest_resolution()
-yt.download()
